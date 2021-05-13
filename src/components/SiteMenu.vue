facdd27733a6001e99bce3b70121f640ac86e557
Adro <acousens@gmail.com>
2021-05-13 13:52:17 -0700

    Rename videos

--- src/components/SiteMenu.vue
+++ src/components/SiteMenu.vue
@@ -2,40 +2,57 @@
   <div>
     <MenuIcon @clicked="toggle" />
     <transition name="fade">
-    <nav v-if="active" class="fixed w-screen h-screen z-40 bg-white right-0 top-0 flex flex-col items-center justify-center overflow-y-auto">
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Bio</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Event</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Contact</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Composer</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Performer</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Scholar</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Videos</g-link>
-      <g-link class="p-4 py-2 block text-center text-3xl" to="/bio">Press</g-link>
-    </nav>
+      <nav
+        v-if="active"
+        class="fixed top-0 right-0 z-40 flex flex-col items-center justify-center w-screen h-screen overflow-y-auto bg-white"
+      >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Bio</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Event</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Composer</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Performer</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Scholar</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/videos/"
+          >Videos</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/press/"
+          >Press</g-link
+        >
+        <g-link class="block p-4 py-2 text-3xl text-center" to="/bio"
+          >Contact</g-link
+        >
+      </nav>
     </transition>
   </div>
 </template>
 
 <script>
-  import MenuIcon from '~/components/MenuIcon'
+import MenuIcon from '~/components/MenuIcon';
 export default {
   name: 'SiteMenu',
   data() {
     return {
-      active: false
-    }
+      active: false,
+    };
   },
-  components: { MenuIcon, },
+  components: { MenuIcon },
   methods: {
     toggle() {
-      this.active = !this.active
-      let bodyClassAction = this.active ? 'add' : 'remove'
+      this.active = !this.active;
+      let bodyClassAction = this.active ? 'add' : 'remove';
       // document.getElementsByTagName('body')[0].classList[bodyClassAction]('overflow-hidden')
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/pages/Videos.vue
@@ -0,0 +1,118 @@
+<template>
+  <Layout>
+    <section>
+      <Heading>Videos </Heading>
+      <div
+        class="flex flex-col items-start flex-initial p-4 space-y-4 video-md:space-y-0 video-md:flex-row video-md:pt-20 video-md:space-x-10"
+      >
+        <div class="relative w-full space-y-4 video-md:w-4/6">
+          <div class="overflow-hidden bg-gray-800 aspect-w-16 aspect-h-9">
+            <transition name="fade" mode="out-in">
+              <component
+                v-bind:is="activePlayer"
+                :video="activeVideo"
+                class="overflow-hidden bg-gray-800 aspect-w-16 aspect-h-9"
+              >
+                >
+              </component>
+            </transition>
+          </div>
+          <div v-if="activeVideo">
+            <h1 class="text-2xl">{{ activeVideo.title }}</h1>
+            <div>{{ activeVideo.description }}</div>
+          </div>
+        </div>
+        <nav class="flex-grow-0 w-full video-md:w-2/6">
+          <ul class="space-y-3">
+            <VideoListItem
+              v-for="video in videos"
+              :key="video.url"
+              @clicked="
+                () => {
+                  setVideo(video);
+                }
+              "
+              :video="video"
+            />
+          </ul>
+        </nav>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+import VideoListItem from '~/components/VideoListItem.vue';
+import YTPlayer from '~/components/YTPlayer.vue';
+import VimeoPlayer from '~/components/VimeoPlayer.vue';
+export default {
+  components: { VimeoPlayer, YTPlayer, VideoListItem },
+  name: 'Videos',
+  data() {
+    return {
+      activePlayer: null,
+      activeVideo: null,
+      videos: [
+        {
+          title: 'youtube video about this thing',
+          url: 'https://www.youtube.com/watch?v=j77wUsGrvrA',
+          description: 'this is the description. areyount you glad??',
+        },
+        {
+          title: 'vimeo video. watch it',
+          url: 'https://vimeo.com/196345588',
+          description: 'this is the description. areyount you glad??',
+        },
+        {
+          title: 'Tips',
+          url: 'https://www.youtube.com/watch?v=u9BoG1n1948',
+          description: 'this is the description. areyount you glad??',
+        },
+        {
+          title: 'frararar',
+          url: 'https://vimeo.com/126060304',
+          description: 'this is the description. areyount you glad??',
+        },
+        {
+          title: 'another youtube',
+          url: 'https://www.youtube.com/watch?v=4n6LrehCPOQ',
+          description: 'this is the description. areyount you glad??',
+        },
+      ],
+    };
+  },
+  methods: {
+    setVideo(video) {
+      console.log('set vid', video);
+      const yt = video.url.includes('youtube');
+      if (yt) {
+        this.activePlayer = YTPlayer;
+      } else {
+        this.activePlayer = VimeoPlayer;
+      }
+      this.activeVideo = video;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.setVideo(this.videos[0]);
+    });
+  },
+  created() {},
+};
+</script>
+
+<style>
+.fade-enter-active {
+  transition: all 0.4s;
+}
+.fade-enter {
+  opacity: 0;
+  /* margin-left: 90px; */
+}
+.fade-leave-active {
+  transition: all 0.4s;
+  opacity: 0;
+  /* margin-left: -100px; */
+}
+</style>
