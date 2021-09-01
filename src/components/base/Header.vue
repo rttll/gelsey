8eff6d78f33d3e1988830d9ee0faca1ce3454304
Adro <acousens@gmail.com>
2021-08-31 19:07:16 -0700

    Fix logo small screens

--- src/components/base/Header.vue
+++ src/components/base/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header
-    class="fixed top-0 left-0 z-40 flex items-start justify-between w-full p-4"
+    class="fixed top-0 left-0 z-40 flex items-start justify-between w-full p-4 bg-white"
   >
     <g-link class="block" to="/">
-      <Logo class="h-24" />
+      <Logo class="h-16 md:h-24" />
     </g-link>
     <SiteMenu />
   </header>
