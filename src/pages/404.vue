62854fa8013b4b0677ac6d26228f323fb23e2598
Adro <acousens@gmail.com>
2021-11-15 12:31:17 -0800

    Add 404

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <Container>
+
+      <Heading :border="false" :smallDisplay="true">
+        404
+      </Heading>
+      <Heading :display="true">
+        page not found
+      </Heading>
+      <article class="pb-6 my-12">
+        <g-link to="/">&larr; Return to homepage</g-link>
+      </article>
+    </Container>
+  </Layout>
+</template>
+
+
+<script>
+export default {
+  name: '404',
+
+};
+</script>
