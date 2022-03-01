6de694a5b1906b1effede8b020ac0f22ec9e715f
Adro <acousens@gmail.com>
2022-03-01 09:48:31 -0800

    idk - events something

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,68 +1,79 @@
 <template>
   <Layout>
-      <header class="">
-        <Heading :display="true">
-          <Container>Events</Container>
-        </Heading>
-      </header>
-    
-    <Container>
-    <section class="p-4 pt-10 space-y-8">
-      <article v-for="event in currentEvents" class="" :key="event.id">
-        <header>
-          <h3
-            class="pb-2 mb-6 text-lg font-semibold text-gray-600 uppercase border-b border-gray-200"
-          >
-            {{ event.title }}
-          </h3>
-        </header>
-        <div class="flex flex-col space-y-8 md:space-y-0 md:space-x-10 md:flex-row pb-9">
-          <div class="space-y-4 md:w-1/3">
-            <div class="flex items-start justify-between space-x-1">
-              <p>{{ event.date_display }}</p>
-              <span
-                v-if="event.ongoing"
-                class="inline-block px-3 text-xs text-white bg-gray-700 rounded-full"
-                style="padding-top: 2px; padding-bottom: 2px"
-              >
-                Ongoing
-              </span>
-            </div>
+    <header class="">
+      <Heading :display="true">
+        <Container>Events</Container>
+      </Heading>
+    </header>
 
-            <div class="space-y-1" v-if="event.address">
-              <h4 class="font-semibold">
-                {{ event.address.name }}
-              </h4>
-              <BlockContent
-                v-if="event.address._rawAddress"
-                class="text-xs"
-                :blocks="event.address._rawAddress" />
-              <a
-                :href="event.address.link"
-                v-if="event.address.link"
-                class="block text-sm text-gray-400 truncate"
-              >
-                {{ event.address.link }}
-              </a>
+    <Container>
+      <section class="p-4 pt-10 space-y-8">
+        <article v-for="event in currentEvents" class="" :key="event.id">
+          <header>
+            <h3
+              class="pb-2 mb-6 text-lg font-semibold text-gray-600 uppercase border-b border-gray-200"
+            >
+              {{ event.title }}
+            </h3>
+          </header>
+          <div
+            class="flex flex-col space-y-8 md:space-y-0 md:space-x-10 md:flex-row pb-9"
+          >
+            <div class="space-y-4 md:w-1/3">
+              <div class="flex items-start justify-between space-x-1">
+                <p>{{ event.date_display }}</p>
+                <span
+                  v-if="event.ongoing"
+                  class="inline-block px-3 text-xs text-white bg-gray-700 rounded-full"
+                  style="padding-top: 2px; padding-bottom: 2px"
+                >
+                  Ongoing
+                </span>
+              </div>
+
+              <div class="space-y-1" v-if="event.address">
+                <h4 class="font-semibold">
+                  {{ event.address.name }}
+                </h4>
+                <BlockContent
+                  v-if="event.address._rawAddress"
+                  class="text-xs"
+                  :blocks="event.address._rawAddress"
+                />
+                <a
+                  :href="event.address.link"
+                  v-if="event.address.link"
+                  class="block text-sm text-gray-400 truncate"
+                >
+                  {{ event.address.link }}
+                </a>
+              </div>
+            </div>
+            <div class="leading-loose md:w-2/3" v-if="event._rawDescription">
+              <BlockContent :blocks="event._rawDescription" />
+              <div v-if="event.link" class="flex justify-end w-full pt-4">
+                <a
+                  :href="event.link"
+                  class="inline-flex items-center space-x-1 font-medium"
+                >
+                  <p class="whitespace-nowrap">Visit site</p>
+                  <span class="text-lg transform -translate-x-1">
+                    <ArrowUpRight />
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
-          <div
-            class="text-sm leading-loose md:w-2/3"
-            v-if="event._rawDescription"
-          >
-            <BlockContent :blocks="event._rawDescription" />
-          </div>
-        </div>
-      </article>
-    </section>
+        </article>
+      </section>
     </Container>
-    
+
     <header class="p-4">
       <Heading :smallDisplay="true">
         <Container>Past Events</Container>
       </Heading>
     </header>
-    
+
     <Container>
       <!-- Past shows -->
       <section class="p-4 pb-0 space-y-8">
@@ -165,7 +176,8 @@ export default {
         }).format(eventDate);
         node.date = formatted.replace(/\//g, '.');
         return node;
-      }).reverse()
+      })
+      .reverse();
 
     if (past.length === 0) return;
 
